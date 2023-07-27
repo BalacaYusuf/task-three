992c5e21f5044b9d7dac3e84284f47d60458cb70
BalacaYusuf <xamoyev.royal1998@gmail.com>
2023-07-27 10:09:54 +0300

    commit

---
+++ task3.js
@@ -0,0 +1,18 @@
+// let arr=[1,2,3,4,5,6];
+
+
+// Array.prototype.myFilter=function(callbackFunc){
+
+
+   
+// let newarray=[];
+// for(let i=0;i<this.length ;i++){
+    
+//         newarray.push(callbackFunc(this[i]))
+    
+// }
+
+// return newarray;
+// }
+
+// console.log(arr.myFilter((callbackelement)=>callbackelement*4));
